f7ef28f4ee911e8ffc17c84b10a6e790a7a6976c
scremmix <alescre01@outlook.com>
2026-04-07 11:29:42 +0200

    Page test

--- src/MainPageContent.js
+++ src/MainPageContent.js
@@ -1,8 +1,8 @@
 function MainPageContent() {
   return (
-    <div>
-        
-    </div>
+    <p style="margin: 10px 15%; width: 66%; padding: 2%; color:#f00; text-align: center;">
+        Content
+    </p>
   );
 }
 
