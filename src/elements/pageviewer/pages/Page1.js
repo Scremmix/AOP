209f836613a7ac66bddfe04ef620ecf7c56d1265
scremmix <alescre01@outlook.com>
2026-04-11 15:53:36 +0200

    Page test for page 1

--- src/elements/pageviewer/pages/Page1.js
+++ src/elements/pageviewer/pages/Page1.js
@@ -2,8 +2,37 @@ import './Page1.css';
 
 function Page1() {
   return (
-    <div>
-        Test Page 1
+    <div className="tabellaMemorialeZenk">
+        <div className="titleRow">
+          <div className="titleElement kodeMono-fontface">Pos.</div>
+          <div className="titleElement kodeMono-fontface">Nome</div>
+          <div className="titleElement kodeMono-fontface">Punteggio</div>
+        </div>
+        <div className="titleRow">
+          <div className="titleElement cuprum-fontface">1</div>
+          <div className="titleElement cuprum-fontface">Albo</div>
+          <div className="titleElement cuprum-fontface">3 pts.</div>
+        </div>
+        <div className="titleRow">
+          <div className="titleElement cuprum-fontface">2</div>
+          <div className="titleElement cuprum-fontface">Matteo</div>
+          <div className="titleElement cuprum-fontface">1 pt.</div>
+        </div>
+        <div className="titleRow">
+          <div className="titleElement cuprum-fontface">3</div>
+          <div className="titleElement cuprum-fontface">Ciccio</div>
+          <div className="titleElement cuprum-fontface">0 pt.</div>
+        </div>
+        <div className="titleRow">
+          <div className="titleElement cuprum-fontface">3</div>
+          <div className="titleElement cuprum-fontface">Lucrezia</div>
+          <div className="titleElement cuprum-fontface">0 pt.</div>
+        </div>
+        <div className="titleRow">
+          <div className="titleElement cuprum-fontface">3</div>
+          <div className="titleElement cuprum-fontface">Screm</div>
+          <div className="titleElement cuprum-fontface">0 pt.</div>
+        </div>
     </div>
   );
 }
