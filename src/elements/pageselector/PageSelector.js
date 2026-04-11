a537a0fbeb4c821ca7b71fb5e7b63e2e8ff8d988
scremmix <alescre01@outlook.com>
2026-04-11 13:24:32 +0200

    Page test for page switching

---
+++ src/elements/pageselector/PageSelector.js
@@ -0,0 +1,22 @@
+import './PageSelector.css';
+
+function PageSelector() {
+  return (
+    <div className="pageSelectorWrapper">
+        <label class="selectorLabel">
+            <input type="radio" id="pageOption1" name="value-radio" defaultValue="Page1" />
+            <span className="text kodeMono-fontface">Classifica Memoriale Zenk</span>
+        </label>
+        <label class="selectorLabel">
+            <input type="radio" id="pageOption2" name="value-radio" defaultValue="Page2" />
+            <span className="text kodeMono-fontface">Classifica Premio Matteo</span>
+        </label>
+        <label class="selectorLabel">
+            <input type="radio" id="pageOption3" name="value-radio" defaultValue="Page3" />
+            <span className="text kodeMono-fontface">Storico</span>
+        </label>
+    </div>
+  );
+}
+
+export default PageSelector;
