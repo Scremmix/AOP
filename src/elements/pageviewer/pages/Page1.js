ac8d8b0095d21760eb965f12070da0914a38174e
scremmix <alescre01@outlook.com>
2026-04-11 16:03:38 +0200

    Page test for page 1

--- src/elements/pageviewer/pages/Page1.js
+++ src/elements/pageviewer/pages/Page1.js
@@ -8,30 +8,30 @@ function Page1() {
           <div className="titleElement kodeMono-fontface">Nome</div>
           <div className="titleElement kodeMono-fontface">Punteggio</div>
         </div>
-        <div className="titleRow">
-          <div className="titleElement cuprum-fontface">1</div>
-          <div className="titleElement cuprum-fontface">Albo</div>
-          <div className="titleElement cuprum-fontface">3 pts.</div>
+        <div className="contentRow">
+          <div className="contentElement cuprum-fontface">1</div>
+          <div className="contentElement cuprum-fontface">Albo</div>
+          <div className="contentElement cuprum-fontface">3 pts.</div>
         </div>
-        <div className="titleRow">
-          <div className="titleElement cuprum-fontface">2</div>
-          <div className="titleElement cuprum-fontface">Matteo</div>
-          <div className="titleElement cuprum-fontface">1 pt.</div>
+        <div className="contentRow">
+          <div className="contentElement cuprum-fontface">2</div>
+          <div className="contentElement cuprum-fontface">Matteo</div>
+          <div className="contentElement cuprum-fontface">1 pt.</div>
         </div>
-        <div className="titleRow">
-          <div className="titleElement cuprum-fontface">3</div>
-          <div className="titleElement cuprum-fontface">Ciccio</div>
-          <div className="titleElement cuprum-fontface">0 pt.</div>
+        <div className="contentRow">
+          <div className="contentElement cuprum-fontface">3</div>
+          <div className="contentElement cuprum-fontface">Ciccio</div>
+          <div className="contentElement cuprum-fontface">0 pt.</div>
         </div>
-        <div className="titleRow">
-          <div className="titleElement cuprum-fontface">3</div>
-          <div className="titleElement cuprum-fontface">Lucrezia</div>
-          <div className="titleElement cuprum-fontface">0 pt.</div>
+        <div className="contentRow">
+          <div className="contentElement cuprum-fontface">3</div>
+          <div className="contentElement cuprum-fontface">Lucrezia</div>
+          <div className="contentElement cuprum-fontface">0 pt.</div>
         </div>
-        <div className="titleRow">
-          <div className="titleElement cuprum-fontface">3</div>
-          <div className="titleElement cuprum-fontface">Screm</div>
-          <div className="titleElement cuprum-fontface">0 pt.</div>
+        <div className="contentRow">
+          <div className="contentElement cuprum-fontface">3</div>
+          <div className="contentElement cuprum-fontface">Screm</div>
+          <div className="contentElement cuprum-fontface">0 pt.</div>
         </div>
     </div>
   );
