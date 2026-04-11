60dc0372cdeb0c7ac5afb0dde4a54693cd001d51
scremmix <alescre01@outlook.com>
2026-04-11 16:46:50 +0200

    Page test for page 2

--- src/elements/pageviewer/pages/Page2.js
+++ src/elements/pageviewer/pages/Page2.js
@@ -2,8 +2,40 @@ import './Page2.css';
 
 function Page2() {
   return (
-    <div>
-        Test Page 2
+    <div className="tabellaPremioMatteo">
+        <div className="subPageTitle kodeMono-fontface">
+          Premio Memoriale Zenk
+        </div>
+        <div className="headerRow">
+          <div className="headerElement kodeMono-fontface">Pos.</div>
+          <div className="headerElement kodeMono-fontface">Nome</div>
+          <div className="headerElement kodeMono-fontface">Punt.</div>
+        </div>
+        <div className="contentRow">
+          <div className="contentElement cuprum-fontface">1</div>
+          <div className="contentElement cuprum-fontface">Matteo</div>
+          <div className="contentElement cuprum-fontface">3 pt.</div>
+        </div>
+        <div className="contentRow">
+          <div className="contentElement cuprum-fontface">2</div>
+          <div className="contentElement cuprum-fontface">Albo</div>
+          <div className="contentElement cuprum-fontface">0 pt.</div>
+        </div>
+        <div className="contentRow">
+          <div className="contentElement cuprum-fontface">2</div>
+          <div className="contentElement cuprum-fontface">Ciccio</div>
+          <div className="contentElement cuprum-fontface">0 pt.</div>
+        </div>
+        <div className="contentRow">
+          <div className="contentElement cuprum-fontface">2</div>
+          <div className="contentElement cuprum-fontface">Lucrezia</div>
+          <div className="contentElement cuprum-fontface">0 pt.</div>
+        </div>
+        <div className="contentRow">
+          <div className="contentElement cuprum-fontface">2</div>
+          <div className="contentElement cuprum-fontface">Screm</div>
+          <div className="contentElement cuprum-fontface">0 pt.</div>
+        </div>
     </div>
   );
 }
