db512400b718548d515e325988bb0d64c57273b5
scremmix <alescre01@outlook.com>
2026-06-20 15:05:14 +0200

    Updated scoreboard

--- src/elements/pageviewer/pages/Page2.js
+++ src/elements/pageviewer/pages/Page2.js
@@ -13,36 +13,41 @@ function Page2() {
         </div>
         <div className="contentRow">
           <div className="contentElement cuprum-fontface">1</div>
+          <div className="contentElement cuprum-fontface">Ciccio</div>
+          <div className="contentElement cuprum-fontface">8 pt.</div>
+        </div>
+        <div className="contentRow">
+          <div className="contentElement cuprum-fontface">2</div>
+          <div className="contentElement cuprum-fontface">Screm</div>
+          <div className="contentElement cuprum-fontface">6 pt.</div>
+        </div>
+        <div className="contentRow">
+          <div className="contentElement cuprum-fontface">3</div>
           <div className="contentElement cuprum-fontface">Matteo</div>
           <div className="contentElement cuprum-fontface">5 pt.</div>
         </div>
         <div className="contentRow">
-          <div className="contentElement cuprum-fontface">1</div>
-          <div className="contentElement cuprum-fontface">Ciccio</div>
-          <div className="contentElement cuprum-fontface">5 pt.</div>
-        </div>
-        <div className="contentRow">
-          <div className="contentElement cuprum-fontface">2</div>
+          <div className="contentElement cuprum-fontface">4</div>
           <div className="contentElement cuprum-fontface">Roda</div>
           <div className="contentElement cuprum-fontface">3 pt.</div>
         </div>
         <div className="contentRow">
-          <div className="contentElement cuprum-fontface">3</div>
+          <div className="contentElement cuprum-fontface">5</div>
           <div className="contentElement cuprum-fontface">Lucrezia</div>
+          <div className="contentElement cuprum-fontface">2 pt.</div>
+        </div>
+        <div className="contentRow">
+          <div className="contentElement cuprum-fontface">6</div>
+          <div className="contentElement cuprum-fontface">Zenk</div>
           <div className="contentElement cuprum-fontface">1 pt.</div>
         </div>
         <div className="contentRow">
-          <div className="contentElement cuprum-fontface">3</div>
-          <div className="contentElement cuprum-fontface">Screm</div>
-          <div className="contentElement cuprum-fontface">1 pt.</div>
-        </div>
-        <div className="contentRow">
-          <div className="contentElement cuprum-fontface">3</div>
+          <div className="contentElement cuprum-fontface">6</div>
           <div className="contentElement cuprum-fontface">Elena</div>
           <div className="contentElement cuprum-fontface">1 pt.</div>
         </div>
         <div className="contentRow">
-          <div className="contentElement cuprum-fontface">4</div>
+          <div className="contentElement cuprum-fontface">7</div>
           <div className="contentElement cuprum-fontface">Albo</div>
           <div className="contentElement cuprum-fontface">0 pt.</div>
         </div>
